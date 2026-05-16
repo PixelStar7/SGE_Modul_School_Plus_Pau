9712a5658878f5ddd1fce34a486397f42a62f824
Pau Tort Balsach <pautortbalsach@gmail.com>
2026-05-16 20:23:21 +0200

    Pràctica 2 Acabada OFICIALMENT :3

--- models/school_plus.py
+++ models/school_plus.py
@@ -3,6 +3,7 @@ from odoo import models, fields, api, _
 from odoo.exceptions import ValidationError 
 from dateutil.relativedelta import relativedelta
 
+# RECORDA LA IDENTACIÓ!!!!!
 
 class SchoolStudent(models.Model):
     _name = 'school.student'
